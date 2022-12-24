255872d6b572a0eb005027510a9caf4765020f81
richardbmezzomo <richardmezzomo1@gmail.com>
2022-12-24 12:06:09 -0300

    feito a logica do js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+const randomNumber = Math.floor(Math.random() * 10)
+let xAttempts = 1
+
 function handleClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
-  const randomNumber = Math.floor(Math.random() * 10)
 
-  console.log(inputNumber.value)
-  console.log(randomNumber)
+  if(Number(inputNumber.value ) == randomNumber) {
+    document.querySelector('.screen1').classList.add('hide')
+    document.querySelector('.screen2').classList.remove('hide')
+
+    document.querySelector('.text-result').innerHTML = `Got it right in ${xAttempts} tries! `
+  }
+  xAttempts++ 
+
 }
