1f756899ca6904554d43051910794384db6f1e0e
richardbmezzomo <richardmezzomo1@gmail.com>
2022-12-27 16:15:43 -0300

    finalizado o jogo

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
+const screen1 = document.querySelector('.screen1')
+const screen2= document.querySelector('.screen2')
 const randomNumber = Math.floor(Math.random() * 10)
 let xAttempts = 1
 
-function handleClick(event) {
+function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
 
-  if(Number(inputNumber.value ) == randomNumber) {
-    document.querySelector('.screen1').classList.add('hide')
-    document.querySelector('.screen2').classList.remove('hide')
+  if(Number(inputNumber.value ) === randomNumber) {
+    screen1.classList.add('hide')
+    screen2.classList.remove('hide')
 
     document.querySelector('.text-result').innerHTML = `Got it right in ${xAttempts} tries! `
   }
+  inputNumber.value = ""
   xAttempts++ 
 
 }
+
+// Eventos
+
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function (){
+  screen1.classList.remove('hide')
+  screen2.classList.add('hide')
+
+  xAttempts = 1
+})
