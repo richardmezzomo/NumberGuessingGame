25cbff05070ef516a3099609fb85f9fea0315624
richardbmezzomo <richardmezzomo1@gmail.com>
2022-12-27 19:23:57 -0300

    corrigido bug do numero nao mudar

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 // Variáveis
 const screen1 = document.querySelector('.screen1')
 const screen2= document.querySelector('.screen2')
-const randomNumber = Math.floor(Math.random() * 10)
 const btnTry = document.querySelector('#btnTry')
 const btnReset = document.querySelector('#btnReset')
+let randomNumber = Math.floor(Math.random() * 10)
 
 let xAttempts = 1
 
-// Function call back
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', keydownReset)
+
+// Functions
 function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
 
-  if(Number(inputNumber.value) === randomNumber) {
+  if(Number(inputNumber.value) == randomNumber) {
     screen1.classList.add('hide')
     screen2.classList.remove('hide')
 
@@ -24,12 +29,18 @@ function handleTryClick(event) {
 }
 
 function handleResetClick() {
-  screen1.classList.remove('hide')
-  screen2.classList.add('hide')
-
+  toggleScreen()
   xAttempts = 1
+  randomNumber = Math.floor(Math.random() * 10)
 } 
 
-// Eventos
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', handleResetClick)
+function toggleScreen() {
+  screen1.classList.remove('hide')
+  screen2.classList.add('hide')
+}
+
+function keydownReset(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+  }
+}
