3bfd414ad20582987b1796f9a5ca4a0f7fb2a05d
richardbmezzomo <richardmezzomo1@gmail.com>
2022-12-27 16:55:00 -0300

    refatorando o JavaScript

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+// Variáveis
 const screen1 = document.querySelector('.screen1')
 const screen2= document.querySelector('.screen2')
 const randomNumber = Math.floor(Math.random() * 10)
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
 let xAttempts = 1
 
+// Function call back
 function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
 
-  if(Number(inputNumber.value ) === randomNumber) {
+  if(Number(inputNumber.value) === randomNumber) {
     screen1.classList.add('hide')
     screen2.classList.remove('hide')
 
@@ -16,18 +21,15 @@ function handleTryClick(event) {
   }
   inputNumber.value = ""
   xAttempts++ 
-
 }
 
-// Eventos
-
-const btnTry = document.querySelector('#btnTry')
-const btnReset = document.querySelector('#btnReset')
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function (){
+function handleResetClick() {
   screen1.classList.remove('hide')
   screen2.classList.add('hide')
 
   xAttempts = 1
-})
+} 
+
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
